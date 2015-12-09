b9f0ad2d30a4fe01ce8e66bb2a4f3d842df5805e
madisontroup <madison.troup@gmail.com>
2015-12-09 11:45:55 -0500

    Initial commit

---
+++ app/src/main/java/com/kiskiarea/madisontroup/periodictablefinal/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kiskiarea.madisontroup.periodictablefinal;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity
+{
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.first_screen);
+            Button b = (Button) findViewById(R.id.beginLearning);
+
+            b.setOnClickListener(
+                    new View.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            startActivity(new Intent(MainActivity.this, PeriodicActivity.class));
+                        }
+                    });
+        }
+}
+
+
+
---
+++ app/src/main/java/com/kiskiarea/madisontroup/periodictablefinal/WebpageActivity.java
@@ -0,0 +1,10 @@
+package com.kiskiarea.madisontroup.periodictablefinal;
+
+/**
+ * Created by madison.troup on 12/9/2015.
+ */
+public class WebpageActivity
+{
+    //startActivity(new Intent(Intent.ACTION_VIEW,
+ //                 Uri.parse("http://www.http://education.jlab.org/itselemental/.com/")));
+}
